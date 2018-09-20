3de627141d18a702f6b1d946e7f538e41558a01f
jevinfangtx <993536978@qq.com>
2018-09-21 00:17:29 +0800

    image fetcher

---
+++ app/src/main/java/com/tencent/effectiveanimation/core/ImageFetcher.java
@@ -0,0 +1,13 @@
+package com.tencent.effectiveanimation.core;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class ImageFetcher {
+
+    private Resources mResources;
+
+    public ImageFetcher(Context context) {
+        mResources = context.getResources();
+    }
+}
